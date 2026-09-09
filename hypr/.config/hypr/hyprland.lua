1fd8255580efb8d8b9f1a1bfaa29e0169b70eb14
Bruno Silva <brunomnsilva@gmail.com>
2026-09-09 18:28:00 +0100

    Show title strip on LibreOffice windows

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -70,3 +70,7 @@ o.window(
     center = true
   }
 )
+
+-- Always show a groupbar-style title strip on LibreOffice documents.
+o.window({ class = "^libreoffice-writer$" }, { group = "new" })
+o.window({ class = "^libreoffice-calc$" },   { group = "new" })
